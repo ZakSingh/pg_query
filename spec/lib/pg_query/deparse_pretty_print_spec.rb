cee67f63a148bed3cc533c4604f341ba6b79b6f6
Lukas Fittl <lukas@fittl.com>
2025-07-01 22:08:15 -0700

    Make Rubocop happy

--- spec/lib/pg_query/deparse_pretty_print_spec.rb
+++ spec/lib/pg_query/deparse_pretty_print_spec.rb
@@ -1,21 +1,22 @@
 require 'spec_helper'
 
 describe PgQuery do
-  let(:oneline_query) { query.gsub(/\s+/, ' ').gsub('( ', '(').gsub(' )', ')').strip.chomp(';') }
-
   describe '.deparse' do
     subject { PgQuery.parse(query).deparse(pretty_print: true) }
 
     context 'SELECT' do
       context 'basic statement' do
-        let(:query) {
-'SELECT a AS b
-FROM x
-WHERE
-    y = 5
-    AND z = y' }
+        let(:query) do
+          <<~Q
+            SELECT a AS b
+            FROM x
+            WHERE
+                y = 5
+                AND z = y
+          Q
+        end
 
-        it { is_expected.to eq query }
+        it { is_expected.to eq query.strip }
       end
     end
   end
